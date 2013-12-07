99b0c96ed4fde459236a87de76255cfd50f38f20
Alex Thomassen <decicus@gmail.com>
2013-12-07 18:25:22 +0100

    Added a hook again, so whenever a Traitor (Hidden), it gets called.

--- source/gamemode/player.lua
+++ source/gamemode/player.lua
@@ -546,6 +546,7 @@ local function CheckCreditAward(victim, attacker)
             for _, ply in pairs(player.GetAll()) do
                if ply:IsActiveTraitor() then
                   ply:AddCredits(amt)
+				  hook.Call("GHCreditAward") --Calls a hook whenever someone get's rewarded.
                end
             end
          end
